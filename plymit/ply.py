11946b69514a20066e38b1170e1f012905dd67f2
Michael Schoenberg <michaelschoenberg@outlook.com>
2016-10-23 21:00:53 -0700

    Early work toward reading ply files as well as writing them; parser for the ply header.

--- plymit/ply.py
+++ plymit/ply.py
@@ -1,6 +1,7 @@
 from enum import Enum, unique
 from collections import namedtuple
 from typing import Union
+from typing import Generator
 import struct
 
 # TODO: This is temporary for testing!
@@ -36,10 +37,8 @@ class ElementPropertyType(Enum):
 
     def encode_instance_to_bytes(self, obj, byte_order):
         if self == ElementPropertyType.FLOAT or self == ElementPropertyType.DOUBLE:
-            # There isn't a native to_bytes method for doubles, irritatingly enough
             format_string = '<' if byte_order == 'little' else '>'
             format_string += 'f' if self == ElementPropertyType.FLOAT else 'd'
-
             return struct.pack(format_string, obj)
         else:
             return obj.to_bytes(self.size_in_bytes, byteorder=byte_order, signed=self.signed)
@@ -57,17 +56,18 @@ def encode_binary_data(obj, endianness, data_type) -> bytearray:
 # noinspection PyInitNewSignature
 @unique
 class PlyFormatOptions(Enum):
-    ASCII = ("ascii", "", encode_ascii_data, " ", "\n", "a+")
-    BINARY_LITTLE_ENDIAN = ("binary_little_endian", "little", encode_binary_data, b"", b"", "a+b")
-    BINARY_BIG_ENDIAN = ("binary_big_endian", "big", encode_binary_data, b"", b"", "a+b")
+    ASCII = ("ascii", "", encode_ascii_data, " ", "\n", "a+", "r")
+    BINARY_LITTLE_ENDIAN = ("binary_little_endian", "little", encode_binary_data, b"", b"", "a+b", "rb")
+    BINARY_BIG_ENDIAN = ("binary_big_endian", "big", encode_binary_data, b"", b"", "a+b", "rb")
 
-    def __init__(self, friendly_name, byte_order, encoder, sep, linesep, filemode):
+    def __init__(self, friendly_name, byte_order, encoder, sep, linesep, write_filemode, read_filemode):
         self.friendly_name = friendly_name
         self.byte_order = byte_order
         self.encoder = encoder
         self.sep = sep
         self.linesep = linesep
-        self.filemode = filemode
+        self.write_filemode = write_filemode
+        self.read_filemode = read_filemode
 
     def concatenate_data(self, elements):
         return self.sep.join(elements)
@@ -96,7 +96,6 @@ class ListProperty(namedtuple('ListProperty', 'name count_type property_type')):
 
     def __str__(self):
         assert(self.count_type != ElementPropertyType.FLOAT and self.count_type != ElementPropertyType.DOUBLE)
-        assert(self.property_type != ElementPropertyType.FLOAT and self.property_type != ElementPropertyType.DOUBLE)
         return str("property list " + self.count_type.friendly_name + " " + self.property_type.friendly_name + " " +
                    self.name + "\n")
 
@@ -122,6 +121,95 @@ class ElementSpecification:
         return ply_format.concatenate_data(elements) + ply_format.linesep
 
 
+def token_stream(file):
+    """Word tokenizer. Also breaks on newlines."""
+    for line in file:
+        for word in line.split(None):
+            yield word
+        yield '\n'
+
+
+class PlyHeaderParser:
+    """A simple parser to read PLY headers and assemble the information therein."""
+    def __init__(self, filename):
+        self.ply_format = None
+        self.elementData = []
+        with open(filename, 'r') as f:
+            words_parsed = 0
+            found_ply_magic_token = False
+            gen = token_stream(f)
+            for word in gen:
+                if word == "ply":
+                    assert(words_parsed == 0)
+                    found_ply_magic_token = True
+                elif word == "format":
+                    self.parse_keyword_format(gen)
+                elif word == "comment":
+                    self.parse_keyword_comment(gen)
+                elif word == "element":
+                    self.parse_keyword_element(gen)
+                elif word == "property":
+                    self.parse_keyword_property(gen)
+                elif word == "end_header":
+                    break
+                words_parsed += 1
+            assert(found_ply_magic_token is True)
+            assert(self.ply_format is not None)
+            self.body_offset = f.tell()
+
+    CountSpecificationPair = namedtuple('CountSpecificationPair', 'count specification')
+
+    @staticmethod
+    def parse_next_word(gen):
+        """Skip the newlines the parser provides."""
+        word = next(gen)
+        while word == '\n':
+            word = next(gen)
+        return word
+
+    def parse_keyword_format(self, gen):
+        assert(self.ply_format is None)
+        format_type = self.parse_next_word(gen)
+        for format_option in PlyFormatOptions:
+            if format_type == format_option.friendly_name:
+                self.ply_format = format_option
+                break
+        version = self.parse_next_word(gen)
+        assert(version == '1.0')
+
+    @staticmethod
+    def parse_keyword_comment(gen):
+        word = ""
+        while word is not '\n':
+            word = next(gen)
+
+    def parse_keyword_element(self, gen):
+        element_name = self.parse_next_word(gen)
+        element_count = int(self.parse_next_word(gen))
+        specification = ElementSpecification(element_name)
+        self.elementData.append(PlyHeaderParser.CountSpecificationPair(element_count, specification))
+
+    def parse_keyword_property(self, gen):
+        assert(self.currentParsingElement is not None)
+        next_word = self.parse_next_word(gen)
+        if next_word == "list":
+            count_property_type = self.get_property_type(self.parse_next_word(gen))
+            property_type = self.get_property_type(self.parse_next_word(gen))
+            property_name = self.parse_next_word(gen)
+            self.elementData[-1].specification.add_property(ListProperty(property_name, count_property_type,
+                                                                         property_type))
+        else:
+            property_type = self.get_property_type(next_word)
+            property_name = self.parse_next_word(gen)
+            self.elementData[-1].specification.add_property(ElementProperty(property_name, property_type))
+
+    @staticmethod
+    def get_property_type(property_type_str):
+        for property_type in ElementPropertyType:
+            if property_type.friendly_name == property_type_str:
+                return property_type
+
+
 class Ply:
     """A simple wrapper containing the data from a Ply file. Ply files contain a list of element types, with a count
     associated with each, and a list of those elements laid out in the order they are specified. Please consult Greg
@@ -130,11 +218,20 @@ class Ply:
         http://www.dcs.ed.ac.uk/teaching/cs4/www/graphics/Web/ply.html
     """
 
-    def __init__(self):
+    def __init__(self, filename=None):
+        """Optionally reads a ply file and populates the internal structures of this object to conform to them.
+        It is important to note that there is no requirement in the ply spec that a ply file contain more than one line.
+        That is, all that is important is that there are separators between the keywords and values."""
         # List of element types, of type ElementSpecification.
         self.elementTypes = []
         # A dictionary keyed by ElementSpecification name containing a list of the actual members of said elements.
         self.elementLists = {}
+
+        if filename is not None:
+            parser = PlyHeaderParser(filename)
+            with open(filename, parser.ply_format.read_filemode) as f:
+                f.seek(parser.body_offset)
+                # TODO: Read the elements!
 
     def add_element_type(self, element_type: ElementSpecification):
         self.elementTypes.append(element_type)
@@ -160,7 +257,7 @@ class Ply:
 
     def write(self, filename, ply_format):
         self.write_header(filename, ply_format)
-        with open(filename, ply_format.filemode) as f:
+        with open(filename, ply_format.write_filemode) as f:
             for et in self.elementTypes:
                 for element in self.elementLists[et.name]:
                     f.write(et.instance_str(element, ply_format))
